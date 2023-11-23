2f40d1b21b588d5b8273d6494f96d25b6ba8a244
Ello Skelling <hello@elloskelling.com>
2023-11-23 10:42:11 -0800

    memer.sh: added some SRT tag cleanup for better search

--- video/memer.sh
+++ video/memer.sh
@@ -12,7 +12,9 @@ FNAME=$(readlink -f "${1}")
 shopt -s extglob
 SRT="$CACHEDIR/${FNAME//+([^[:alnum:][:blank:]])/_}".srt
 
-if [ -f "$SRT" ] || ffmpeg -i "${1}" -y -loglevel quiet -map 0:s:0 "${SRT}"; then
+# best tag stripper ever
+# we clean up the subtitles (remove newlines and markup) only for the search. Later, we'll burn them with original formatting
+if [ -f "$SRT" ] || ffmpeg -i "${1}" -y -loglevel quiet -map 0:s:0 -f srt -|perl -lnpe 's/\{.*?\}//g;s/\<.*?\>//g;'|perl -pge 's/\r\n/\ /g' > "${SRT}"; then
 	# srt_avg will only pull the last matching result
 	ts=$(grep -iC4 "${2}" "${SRT}" |perl -pge "s/.*\n\d+\n(.*\d)\n.*\Q\$ENV{'2'}\E.*\d+\n.*/\1/gsi and s/ --> /\n/g"|srt_avg.py);
 	if ffmpeg -ss "${ts}" -copyts -i "${1}" -frames:v 1 -ss "${ts}" -vf subtitles="${1}" -loglevel quiet -y "${IMGOUT}"; then
