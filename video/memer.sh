cdee84be488ad5ddaec13e9dfcb5d58485da0587
Ello Skelling <hello@elloskelling.com>
2023-11-22 22:25:32 -0800

    added srt caching to memer

--- video/memer.sh
+++ video/memer.sh
@@ -1,12 +1,19 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 : ${1?"Usage: $0 video_with_subs sub_search_string"}
 
-SRT=$(mktemp --suffix .srt /tmp/srt.XXXXX)
+CACHEDIR="$HOME/.memer"
+mkdir -p "$CACHEDIR"
 
 IMGOUT=$(basename "${1%.*}-${2}".jpg)
 
-if ffmpeg -i "${1}" -y -loglevel quiet -map 0:s:0 "${SRT}"; then
+# best cache hack ever
+FNAME=$(readlink -f "${1}")
+shopt -s extglob
+SRT="$CACHEDIR/${FNAME//+([^[:alnum:][:blank:]])/_}".srt
+
+if [ -f "$SRT" ] || ffmpeg -i "${1}" -y -loglevel quiet -map 0:s:0 "${SRT}"; then
+	# srt_avg will only pull the last matching result
 	ts=$(grep -iC4 "${2}" "${SRT}" |perl -pge "s/.*\n\d+\n(.*\d)\n.*\Q\$ENV{'2'}\E.*\d+\n.*/\1/gsi and s/ --> /\n/g"|srt_avg.py);
 	if ffmpeg -ss "${ts}" -copyts -i "${1}" -frames:v 1 -ss "${ts}" -vf subtitles="${1}" -loglevel quiet -y "${IMGOUT}"; then
 		open "${IMGOUT}";
@@ -18,6 +25,5 @@ else
 	#echo Failed to extract SRT
 	exit 1
 fi
-rm "${SRT}"
 
 
