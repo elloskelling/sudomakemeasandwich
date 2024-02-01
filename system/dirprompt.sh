a2d0b656f7aa43051b5945cc07d1c4396b98bb69
Ello Skelling <hello@elloskelling.com>
2024-02-01 08:58:48 -0800

    added optional/toggleable path truncation for dirstack header

--- system/dirprompt.sh
+++ system/dirprompt.sh
@@ -12,8 +12,14 @@ zstyle ':completion:*:directory-stack' list-colors '=(#b) #([0-9]#)*( *)==95=38;
 
 export _qdirPS1=$PS1
 export _qdirCOLS=$COLUMNS	
+# unset this to turn off path truncation. Works dynamically so you can toggle it in a running shell
+export _qdirTRUNCATE=1
 get_qdirs() { # ennumerate and pretty-print the dirs and fill the column width
 	echo -ne "\e[90;1;3m";
-	echo -ne $(dirs | perl -pe 's/(\S+)/($i++) . ":$1"/ge;$_=substr($_,0,$ENV{_qdirCOLS});s/(\d+:)/\e[90;1;3m$1\e[2;3;38;5;238m/g')
+	if [[ -z $_qdirTRUNCATE ]]; then
+		echo -ne $(dirs | perl -pe 's/(\S+)/($i++) . ":$1"/ge;$_=substr($_,0,$ENV{_qdirCOLS});s/(\d+:)/\e[90;1;3m$1\e[2;3;38;5;238m/g')
+	else
+		echo -ne $(dirs | perl -pe 's/(\S{13})\S*(\S{13})\b/$1…$2/g;s/(\S+)/($i++) . ":$1"/ge;$_=substr($_,0,$ENV{_qdirCOLS});s/(\d+:)/\e[90;1;3m$1\e[2;3;38;5;238m/g')
+	fi
 }
 PS1=$_qdirPS1$'%{\e[s\e[H$(get_qdirs)\e[u%}' 
